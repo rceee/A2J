e7cd0a5f423a3ca2c55aeadfd91c1e24ebbd9f53
rceee <rc@rcdmcg.com>
2013-04-06 14:38:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+A2j::Application.config.secret_token = '514d7d415f72845750350ba217fe73efe0192b22f4b83e8b43b8ffaa4d6a683402b6f803cc4a31d837af7f240a0edfc7b03b9317d76e5f3062ab37dc6ee59242'
