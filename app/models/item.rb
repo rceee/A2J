0c3f63aef59f65b89cbb45ee8b1ceb86f418964e
rceee <rc@rcdmcg.com>
2013-04-07 13:54:11 -0500

    Improved Amazon link (direct to music section) on user input form

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ActiveRecord::Base
   attr_accessible :description, :name, :release, :salesrank, :thumbnail_url, :album, :asin
 
+  validates_presence_of :asin
+
   def self.lookup(id)
     res = Amazon::Ecs.item_lookup(id, :response_group => 'ItemAttributes,Images,EditorialReview,SalesRank')
   end
